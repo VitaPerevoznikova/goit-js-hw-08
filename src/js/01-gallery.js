ddbf1457e5ab7239c05b9d227ec3816b9c475061
VitaPerevoznikova <frayvita@gmail.com>
2023-08-26 12:57:27 +0300

    update js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,16 @@
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 import { galleryItems } from './gallery-items';
-console.log(galleryItems);
+
 
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
+
 function createGalleryItemsMarkup(items) {
   return items.map(({ preview, original, description }) => {
-    return `<li>
+    return `
   <a class="gallery__item" href="${original}">
     <img
       class="gallery__image"
@@ -18,9 +18,10 @@ function createGalleryItemsMarkup(items) {
       alt="${description}"
     />
   </a>
-</li>`
+`
   }).join('');
 }
+
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
